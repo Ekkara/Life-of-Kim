f5ce9d174595df61832326a8f08587fc25467ca4
helheim0 <helgu77@yahoo.com>
2021-11-23 23:52:56 +0100

    Dialogues

--- Serious games project/Assets/MenuController.cs
+++ Serious games project/Assets/MenuController.cs
@@ -7,7 +7,7 @@ public class MenuController : MonoBehaviour
 {
    public void PlayGame()
    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1 );
+        SceneManager.LoadScene("BedRoom");
    }
 
     public void QuitGame()
